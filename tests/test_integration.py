e1e9b160e78d9ef391d870865f0d3898711a15d5
justinGmail <justin.delvecchio@gmail.com>
2024-10-04 15:43:52 -0400

    Added more configuation.

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -3,7 +3,7 @@ import requests
 
 class TestFlaskIntegration(unittest.TestCase):
     def test_hello_endpoint(self):
-        response = requests.get('http://localhost:5005/')
+        response = requests.get('http://localhost:5000/')
         self.assertEqual(response.status_code, 200)
         self.assertIn('Hello, World!', response.json()['message'])
 
